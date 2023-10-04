dd583eb1283c13c235541d26f6cdb7da40ebab03
VinaySavla <savlavinay77@gmail.com>
2023-10-04 16:10:27 +0530

    removed unnecessary files

--- eppo/eppo-backend/Final APIS/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con=mysqli_connect('mysql.menrvasolutions.com','se51','Sakec@1234','eppodb');
-// mysqli_query($con,"INSERT into otp(mobile_no,otp) values('12345678','2345678')");
-?>
--- eppo/eppo-backend/Final APIS/demo.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once('verifyOtpApi.php');
-verifyOTP(8422060813, 3013, 'Sanket Ugale', 'password', 'DADAR', 'sanketugale2003@gmail.com', 'Mumbai');
-?>
